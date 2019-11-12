5112ae718e849bf333fb4e9300dab9f577544946
root <root@srvhome.local>
2019-11-12 07:58:39 +0300

    Change php/Dockerfile

--- php/cron.php
+++ php/cron.php
@@ -1,4 +1,4 @@
-# permission must be
+# permission must have
 # -rw------- 1 root 295 Nov 0 22:41 cron.php
 
 SHELL=/bin/sh
@@ -7,5 +7,3 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 14 */6 * * * /usr/local/bin/php /eset_mirror_script/update.php
 @reboot /usr/local/bin/php /eset_mirror_script/update.php
 
-#* * * * * /usr/local/bin/php /eset_mirror_script/update.php
-#* * * * * echo 111 >> /1.txt
