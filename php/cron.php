1d74b11fd66777afc302653e02e944837dd4de7c
root <root@srvhome.local>
2020-02-20 23:37:43 +0300

    Change php/cron.php

--- php/cron.php
+++ php/cron.php
@@ -4,7 +4,14 @@
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-14 */6 * * * /usr/local/bin/php /eset_mirror_script/update.php
+# Удаление базы
+1 1 * * 1 rm -rf "/nod32-base/v*"
+
+# Запуск с задержкой скрипта обновления
+14 1/6 * * * sleep `/usr/bin/od -An -N1 -i /dev/urandom`; /usr/local/bin/php /eset_mirror_script/update.php
+
+# Запуск при старте контейнера
 @reboot /usr/local/bin/php /eset_mirror_script/update.php
+
 @reboot [ ! -f /nod32-base/favicon.ico  ] && cp /favicon.ico /nod32-base/favicon.ico
 
